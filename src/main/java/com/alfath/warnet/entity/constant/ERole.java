adc5ee49dc9ff2d58eb3cbf5e0bfdedc09342411
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-02 23:02:22 +0700

    [FEATURE] Add Role

---
+++ src/main/java/com/alfath/warnet/entity/constant/ERole.java
@@ -0,0 +1,15 @@
+package com.alfath.warnet.entity.constant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public enum ERole {
+    ROLE_ADMIN;
+
+    public static ERole get(String value) {
+        for (ERole eRole : ERole.values()) {
+            if (eRole.name().equalsIgnoreCase(value)) return eRole;
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "role not found");
+    }
+}
